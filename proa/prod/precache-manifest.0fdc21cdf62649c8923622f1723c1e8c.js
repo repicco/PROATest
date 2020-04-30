3079597b86ef439f669a64ef798d1e320f9c71bc
Renato Picco <56326785+repicco@users.noreply.github.com>
2020-04-30 03:15:49 -0300

    PROATest.prod

---
+++ proa/prod/precache-manifest.0fdc21cdf62649c8923622f1723c1e8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28aebfa9afe2059f38c286270166fcd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50740b81041409d17063",
+    "url": "/static/css/main.7548da8a.chunk.css"
+  },
+  {
+    "revision": "013776ec686d553dd717",
+    "url": "/static/js/2.0db848bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0db848bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50740b81041409d17063",
+    "url": "/static/js/main.381caaaf.chunk.js"
+  },
+  {
+    "revision": "87a2fdda5d086fc7767f",
+    "url": "/static/js/runtime-main.16d86b6d.js"
+  },
+  {
+    "revision": "e4828b92fb19cc0c098bad1eb5ed9b03",
+    "url": "/static/media/picanto.e4828b92.jpg"
+  }
+]);
